328e0ff6b7625198f1d8158d330b6508e85de60b
tony-albanese <tony.albanese2000@gmail.com>
2019-12-25 19:58:41 +0100

    Fixed bug.

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main );
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
         this.configureTextViewMain();
@@ -22,13 +22,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void configureTextViewMain(){
-        this.textViewMain.setTextSize(15);
-        this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
+        this.textViewMain.setTextSize(15f);
+        this.textViewMain.setText("Real Estate App");
     }
 
     private void configureTextViewQuantity(){
         int quantity = Utils.convertDollarToEuro(100);
         this.textViewQuantity.setTextSize(20);
-        this.textViewQuantity.setText(quantity);
+        this.textViewQuantity.setText(Integer.toString(quantity));
     }
 }
