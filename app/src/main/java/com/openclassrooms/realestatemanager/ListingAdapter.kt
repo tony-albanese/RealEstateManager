1562d811e2d0dcda0a5b85a18da025abeb088d23
tony-albanese <tony.albanese2000@gmail.com>
2019-12-26 00:13:45 +0100

    Implemented standard functions.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingAdapter.kt
@@ -1,25 +1,36 @@
 package com.openclassrooms.realestatemanager
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.database_files.Listing
 
 class ListingAdapter : RecyclerView.Adapter<ListingAdapter.ListingViewHolder>() {
 
+    private var listings = emptyList<Listing>()
     inner class ListingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        
+        val listingItemView = itemView.findViewById<TextView>(R.id.tv_listing_text)
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListingViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.listing_item_layout, parent, false)
+        return ListingViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listings.size
+    }
+
+    internal fun setListings(listings: List<Listing>) {
+        this.listings = listings
+        notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(holder: ListingViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val current = listings[position]
+        holder.listingItemView.text = current.listingDescription
     }
 }
