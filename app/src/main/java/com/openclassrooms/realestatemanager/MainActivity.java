5584fff36d585545072f2086a2d76bbff166455a
tony-albanese <tony.albanese2000@gmail.com>
2019-12-25 20:03:01 +0100

    Migrated to Androidx

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -1,8 +1,9 @@
 package com.openclassrooms.realestatemanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
