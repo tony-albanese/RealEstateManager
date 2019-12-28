5654c0f5b236684d5f127767c08c97bf28f2ceb0
tony-albanese <tony.albanese2000@gmail.com>
2019-12-28 12:40:52 +0100

    Implemented get item details.

--- app/src/main/java/com/openclassrooms/realestatemanager/recycler_selection/Lookup.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/recycler_selection/Lookup.kt
@@ -4,10 +4,14 @@ import android.view.MotionEvent
 import androidx.recyclerview.selection.ItemDetailsLookup
 import androidx.recyclerview.widget.RecyclerView
 
-class Lookup(val rv: RecyclerView) : ItemDetailsLookup<String>() {
-    override fun getItemDetails(e: MotionEvent): ItemDetails<String>? {
+class Lookup(val rv: RecyclerView) : ItemDetailsLookup<Long>() {
+    override fun getItemDetails(e: MotionEvent): ItemDetails<Long>? {
 
+        val view = rv.findChildViewUnder(e.x, e.y)
+        if (view != null) {
+            val holder = rv.getChildViewHolder(view) as ListingAdapter.ListingViewHolder
+            return holder.getItemDetails()
+        }
         return null
     }
-
 }
