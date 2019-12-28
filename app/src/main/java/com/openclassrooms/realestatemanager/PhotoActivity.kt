121e9e226354a2ab528dcc7d10656736f2adb441
tony-albanese <tony.albanese2000@gmail.com>
2019-12-28 13:01:08 +0100

    Added tracker and passed it to the adapter.

--- app/src/main/java/com/openclassrooms/realestatemanager/PhotoActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/PhotoActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StableIdKeyProvider
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.openclassrooms.realestatemanager.database_files.ListingViewModel
 import com.openclassrooms.realestatemanager.recycler_selection.ListingAdapter
+import com.openclassrooms.realestatemanager.recycler_selection.Lookup
 
 class PhotoActivity : AppCompatActivity() {
 
     lateinit var listingViewModel: ListingViewModel
+    var tracker: SelectionTracker<Long>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +36,16 @@ class PhotoActivity : AppCompatActivity() {
             }
         })
 
+        tracker = SelectionTracker.Builder<Long>(
+                "selection-id",
+                recyclerView,
+                StableIdKeyProvider(recyclerView),
+                Lookup(recyclerView),
+                StorageStrategy.createLongStorage()
+        ).withSelectionPredicate(
+                SelectionPredicates.createSelectAnything()
+        ).build()
+
+        adapter.setTracker(tracker)
     }
 }
