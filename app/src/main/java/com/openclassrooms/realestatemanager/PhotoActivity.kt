b666752c60fa77bbf39bb2d414990a60b8734511
tony-albanese <tony.albanese2000@gmail.com>
2019-12-28 12:09:59 +0100

    Added listing view.

--- app/src/main/java/com/openclassrooms/realestatemanager/PhotoActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/PhotoActivity.kt
@@ -2,11 +2,32 @@ package com.openclassrooms.realestatemanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.database_files.ListingViewModel
 
 class PhotoActivity : AppCompatActivity() {
+
+    lateinit var listingViewModel: ListingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_listings)
+        val adapter = ListingAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        listingViewModel = ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(application)).get(ListingViewModel::class.java)
+
+        listingViewModel.listings.observe(this, Observer {
+            it?.let {
+                adapter.setListings(it)
+            }
+        })
+
     }
 }
