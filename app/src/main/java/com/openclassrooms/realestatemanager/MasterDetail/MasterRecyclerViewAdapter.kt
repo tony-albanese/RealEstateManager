e040c848130be28e70a2b453f8f0a9a46da7a76d
tony-albanese <tony.albanese2000@gmail.com>
2020-01-25 11:25:36 +0100

    Added constructor and implemented getItemCount()

--- app/src/main/java/com/openclassrooms/realestatemanager/MasterDetail/MasterRecyclerViewAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MasterDetail/MasterRecyclerViewAdapter.kt
@@ -3,8 +3,15 @@ package com.openclassrooms.realestatemanager.MasterDetail
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.database_files.Listing
 
-class MasterRecyclerViewAdapter : RecyclerView.Adapter<MasterRecyclerViewAdapter.ListingViewHolder>() {
+class MasterRecyclerViewAdapter(
+        private val parentActivity: ListingListActivity,
+        private val twoPane: Boolean
+) : RecyclerView.Adapter<MasterRecyclerViewAdapter.ListingViewHolder>() {
+
+
+    private var listings = emptyList<Listing>()
 
 
     inner class ListingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -16,7 +23,7 @@ class MasterRecyclerViewAdapter : RecyclerView.Adapter<MasterRecyclerViewAdapter
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listings.size
     }
 
     override fun onBindViewHolder(holder: ListingViewHolder, position: Int) {
