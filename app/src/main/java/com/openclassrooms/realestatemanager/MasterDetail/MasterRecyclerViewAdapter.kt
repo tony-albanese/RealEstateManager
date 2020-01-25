e0c5122615f0b96a7a8c8c51b288ccf3e6cbcc00
tony-albanese <tony.albanese2000@gmail.com>
2020-01-25 12:01:28 +0100

    Setup init block for the on click listener.

--- app/src/main/java/com/openclassrooms/realestatemanager/MasterDetail/MasterRecyclerViewAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MasterDetail/MasterRecyclerViewAdapter.kt
@@ -1,8 +1,11 @@
 package com.openclassrooms.realestatemanager.MasterDetail
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.database_files.Listing
 
 class MasterRecyclerViewAdapter(
@@ -12,8 +15,28 @@ class MasterRecyclerViewAdapter(
 
 
     private var listings = emptyList<Listing>()
+    private val onClickListener: View.OnClickListener
 
-
+    init {
+        onClickListener = View.OnClickListener { clickedView: View ->
+            val listingItem = clickedView.tag as Listing
+            if (twoPane) {
+                val fragment = ListingDetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putLong(ListingDetailFragment.ARG_ITEM_ID, listingItem.id)
+                    }
+                }
+                parentActivity.supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.listing_detail_container, fragment)
+                        .commit()
+            } else {
+                val intent = Intent(clickedView.context, ListingDetailActivity::class.java)
+                intent.putExtra(ListingDetailFragment.ARG_ITEM_ID, listingItem.id)
+                clickedView.context.startActivity(intent)
+            }
+        }
+    }
     inner class ListingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
