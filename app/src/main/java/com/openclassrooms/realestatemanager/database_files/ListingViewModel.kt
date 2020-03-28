70d556546f0a542ecded8996fe71a8f820f1e30b
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 11:44:54 +0100

    Copied code to setup ListingViewModel

--- app/src/main/java/com/openclassrooms/realestatemanager/database_files/ListingViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/ListingViewModel.kt
@@ -1,4 +1,34 @@
 package com.openclassrooms.realestatemanager.database_files
 
-class ListingViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ListingViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: ListingRepository
+    val listings: LiveData<List<Listing>>
+
+    init {
+        val listingDao = AppDatabase.getDatabase(application).listingDao()
+        repository = ListingRepository(listingDao)
+        listings = repository.allListings
+    }
+
+
+    fun insert(listing: Listing) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(listing)
+    }
+
+    fun clearDatabase() = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete()
+    }
+
+    fun getListingById(id: Long) = viewModelScope.launch(Dispatchers.IO) {
+        repository.getListingById(id)
+        //TODO: How to get return value out of the coroutine scope.
+    }
 }
