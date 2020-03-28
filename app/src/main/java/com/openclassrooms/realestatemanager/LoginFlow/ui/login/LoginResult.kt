eb93d6007951ab7fbd095a5fdbcbaf71855d4c52
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 07:49:23 +0100

    Added files for login.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/LoginFlow/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.openclassrooms.realestatemanager.LoginFlow.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
