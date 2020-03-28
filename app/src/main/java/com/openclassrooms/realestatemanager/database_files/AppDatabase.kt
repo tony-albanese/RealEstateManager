772fb841839d8a552e33724614e3021e1b377fb8
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 11:38:09 +0100

    Created companion object to create an instance of the database if it is not already created.

--- app/src/main/java/com/openclassrooms/realestatemanager/database_files/AppDatabase.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/AppDatabase.kt
@@ -1,9 +1,36 @@
 package com.openclassrooms.realestatemanager.database_files
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Listing::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun listingDao(): ListingDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "listing-database"
+                )
+                        .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
 }
