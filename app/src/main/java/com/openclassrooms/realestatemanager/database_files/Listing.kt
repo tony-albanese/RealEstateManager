c36115ab3897d0f061800ff7be62da8f26f4015a
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 11:02:24 +0100

    Added required fields to Listing object.

--- app/src/main/java/com/openclassrooms/realestatemanager/database_files/Listing.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/Listing.kt
@@ -7,6 +7,17 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "table_listings")
 data class Listing(
         @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
-        @ColumnInfo(name = "listing_description") var listingDescription: String = ""
+        @ColumnInfo(name = "listing_description") var listingDescription: String = "",
+        @ColumnInfo(name = "listing_type") var listingType: String = "",
+        @ColumnInfo(name = "listing_area") var listingArea: Int = 0,
+        @ColumnInfo(name = "listing_price") var listingPrice: Int = 0,
+        @ColumnInfo(name = "total_rooms") var numberOfRooms: Int = 0,
+        @ColumnInfo(name = "number_beddrooms") var numberOfBedrooms: Int = 0,
+        @ColumnInfo(name = "isAvailable") var isAvailable: Boolean = true,
+        @ColumnInfo(name = "listing_date") var listingDate: String = "",
+        @ColumnInfo(name = "listing_sale_date") var listingSaleDate: String = "",
+        @ColumnInfo(name = "listing_agent_id") var listingAgentId: Long = 0
+
+        //TODO: Still need a way to record POIs.
 ) {
 }
