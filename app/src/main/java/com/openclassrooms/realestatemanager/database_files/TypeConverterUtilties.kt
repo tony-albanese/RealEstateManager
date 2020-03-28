0bd6584723901cfc492f6896944f2fe866e397d2
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 11:32:59 +0100

    Initial commit.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/TypeConverterUtilties.kt
@@ -0,0 +1,7 @@
+package com.openclassrooms.realestatemanager.database_files
+
+/*
+This class will provide the coversion utitilities needed to store complex objects in Room.
+ */
+class TypeConverterUtilities {
+}
