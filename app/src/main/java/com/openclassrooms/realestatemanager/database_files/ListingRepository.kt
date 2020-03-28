b65d27242514c7d8a24477db6f3ec64401f52717
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 11:22:12 +0100

    Initital commit. Added suspend functions to carry out queries.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/ListingRepository.kt
@@ -0,0 +1,20 @@
+package com.openclassrooms.realestatemanager.database_files
+
+class ListingRepository(
+        private val listingDao: ListingDao
+) {
+    val allListings = listingDao.getAllListings()
+
+    suspend fun insert(listing: Listing) {
+        listingDao.insertListing(listing)
+    }
+
+    suspend fun delete() {
+        listingDao.deleteAllListings()
+    }
+
+    suspend fun getListingById(id: Long): Listing {
+        return listingDao.getListingById(id)
+    }
+
+}
