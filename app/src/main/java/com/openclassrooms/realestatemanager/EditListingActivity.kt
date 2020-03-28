38782a62060b898a4477ef18326691dc962bc5b4
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 17:29:43 +0100

    Set the spinner array for the listing types.

--- app/src/main/java/com/openclassrooms/realestatemanager/EditListingActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/EditListingActivity.kt
@@ -1,6 +1,8 @@
 package com.openclassrooms.realestatemanager
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class EditListingActivity : AppCompatActivity() {
@@ -8,5 +10,19 @@ class EditListingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.listing_edit_layout)
+
+        val spinner = findViewById<Spinner>(R.id.spinner_listing_type)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.listing_type_array,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
 }
