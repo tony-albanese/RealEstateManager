61ba14d76e5604ac8101797b1c6cca6d7beb6e57
tony-albanese <tony.albanese2000@gmail.com>
2020-03-28 17:16:08 +0100

    Initial commit. Inflated listing edit activity layout.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/EditListingActivity.kt
@@ -0,0 +1,12 @@
+package com.openclassrooms.realestatemanager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditListingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.listing_edit_layout)
+    }
+}
