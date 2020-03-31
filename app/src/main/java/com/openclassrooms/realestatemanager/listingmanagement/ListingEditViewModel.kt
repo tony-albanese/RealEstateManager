8bdf111c976e47ef46e248c056a0e391f192597e
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 15:26:05 +0200

    Added methods to save and retrieve the number of rooms and the number of bedrooms in a listing object.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -3,7 +3,6 @@ package com.openclassrooms.realestatemanager.listingmanagement
 import android.app.Application
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.openclassrooms.realestatemanager.Utilities.SPINNER_POSITION_KEY
 import com.openclassrooms.realestatemanager.database_files.AppDatabase
 import com.openclassrooms.realestatemanager.database_files.ListingRepository
 
@@ -17,6 +16,9 @@ class ListingEditViewModel(
         private val handle: SavedStateHandle
 ) : ViewModel() {
 
+    val SPINNER_POSITION_KEY: String = "spinner_position_key"
+    val TOTAL_ROOMS_KEY: String = "total_rooms_key"
+    val BEDROOMS_KEY: String = "bedrooms_key"
 
     var streetAddress: String
     var zipCode: String
@@ -38,12 +40,29 @@ class ListingEditViewModel(
     }
 
 
-    fun setSpinnerPosition(position: Int) {
+    fun saveSpinnerPosition(position: Int) {
         handle.set(SPINNER_POSITION_KEY, position)
     }
 
     fun getSpinnerPosition(): Int {
         return handle.get(SPINNER_POSITION_KEY) ?: 0
+    }
+
+    fun saveNumberOfRooms(rooms: Int) {
+        handle.set(TOTAL_ROOMS_KEY, rooms)
+    }
+
+
+    fun getNumberOfRooms(): Int {
+        return handle.get(TOTAL_ROOMS_KEY) ?: 0
+    }
+
+    fun saveNumberOfBedroom(bedrooms: Int) {
+        handle.set(BEDROOMS_KEY, bedrooms)
+    }
+
+    fun getBedrooms(): Int {
+        return handle.get(BEDROOMS_KEY) ?: 0
     }
 
 }
