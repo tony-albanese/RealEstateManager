f170a7133569a9481d8a22de7d56819fb125236d
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 12:20:05 +0200

    Added handle to constructor and extended ViewModel class.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -1,9 +1,18 @@
 package com.openclassrooms.realestatemanager.listingmanagement
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
 /*
 This class is designed to preserve the UI state when editing
 a listing.
  */
 
-class ListingEditViewModel {
+class ListingEditViewModel(
+        private val handle: SavedStateHandle
+) : ViewModel() {
+
+    fun loadData() {
+        
+    }
 }
