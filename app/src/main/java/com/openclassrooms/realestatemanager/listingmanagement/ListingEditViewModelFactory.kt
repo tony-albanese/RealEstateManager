418ac0f96dbb50cbded78a4580c50c3b09799f89
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 13:41:02 +0200

    Initial commit.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModelFactory.kt
@@ -0,0 +1,10 @@
+package com.openclassrooms.realestatemanager.listingmanagement
+
+/*
+This class is designed to create and EditViewModel so that parameters can be
+passed to it.
+ */
+class ListingEditViewModelFactory {
+
+
+}
