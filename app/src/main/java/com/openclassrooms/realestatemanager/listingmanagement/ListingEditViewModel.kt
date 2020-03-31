2c58acfb7f004f3a698e390378d3c7e997dda18e
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 12:18:00 +0200

    Initial commit.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -0,0 +1,9 @@
+package com.openclassrooms.realestatemanager.listingmanagement
+
+/*
+This class is designed to preserve the UI state when editing
+a listing.
+ */
+
+class ListingEditViewModel {
+}
