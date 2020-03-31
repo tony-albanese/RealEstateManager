c4c287b9a09b9cc0f3c99c73ab68076bc68dd649
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 17:18:37 +0200

    Implemented skeleton of a datepicker dialog.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingDatePicker.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingDatePicker.kt
@@ -5,10 +5,11 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.widget.DatePicker
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
-class ListingDatePicker(val c: Context, val calendar: Calendar) : DialogFragment(), DatePickerDialog.OnDateSetListener {
+class ListingDatePicker(val c: Context, val calendar: Calendar, val tv: TextView, val callback: (TextView, String) -> Unit) : DialogFragment(), DatePickerDialog.OnDateSetListener {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return DatePickerDialog(c,
@@ -18,7 +19,10 @@ class ListingDatePicker(val c: Context, val calendar: Calendar) : DialogFragment
                 calendar.get(Calendar.DAY_OF_MONTH))
     }
 
+    /*
+    TODO: Format the date properly and update the textview.
+     */
     override fun onDateSet(picker: DatePicker?, year: Int, month: Int, day: Int) {
-
+        callback(tv, "Picker callback called.")
     }
 }
