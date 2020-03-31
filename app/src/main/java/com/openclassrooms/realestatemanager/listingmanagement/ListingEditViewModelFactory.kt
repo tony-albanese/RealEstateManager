0531a0eb6deabc8c964695d3a047e4ccc9f9de82
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 13:46:30 +0200

    Set up ViewModelFactory

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModelFactory.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModelFactory.kt
@@ -1,10 +1,27 @@
 package com.openclassrooms.realestatemanager.listingmanagement
 
+import android.app.Application
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+
 /*
 This class is designed to create and EditViewModel so that parameters can be
 passed to it.
  */
-class ListingEditViewModelFactory {
-
+class ListingEditViewModelFactory(
+        val application: Application,
+        val owner: SavedStateRegistryOwner,
+        defaultArgs: Bundle? = null
+) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+    override fun <T : ViewModel> create(
+            key: String,
+            modelClass: Class<T>,
+            handle: SavedStateHandle
+    ): T {
+        return ListingEditViewModel(application, handle) as T
+    }
 
 }
