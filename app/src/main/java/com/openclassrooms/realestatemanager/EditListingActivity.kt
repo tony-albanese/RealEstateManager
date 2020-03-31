74836fe263d39cca651caaf00318e4436272fa79
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 15:25:22 +0200

    Saved UI state for the bedrooms, total rooms, spinner position.

--- app/src/main/java/com/openclassrooms/realestatemanager/EditListingActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/EditListingActivity.kt
@@ -15,6 +15,7 @@ import kotlinx.android.synthetic.main.listing_form_input_layout.*
 class EditListingActivity : AppCompatActivity() {
 
     lateinit var viewModel: ListingEditViewModel
+    lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +24,7 @@ class EditListingActivity : AppCompatActivity() {
         edit_listing_toolbar.title = "Edit Listing"
         setSupportActionBar(edit_listing_toolbar)
 
-        val spinner = findViewById<Spinner>(R.id.spinner_listing_type)
+        spinner = findViewById<Spinner>(R.id.spinner_listing_type)
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
@@ -53,8 +54,28 @@ class EditListingActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onPause() {
+        super.onPause()
+        saveUI()
+    }
+
+
     fun loadUI() {
         et_listing_street_address.setText(viewModel.streetAddress)
+        et_listing_city.setText(viewModel.city)
+        et_listing_zipcode.setText(viewModel.zipCode)
+        et_listing_sales_price.setText(viewModel.salesPrice)
+
+        spinner.setSelection(viewModel.getSpinnerPosition())
+        seekbar_total_rooms.progress = viewModel.getNumberOfRooms()
+        seekbar_bedrooms.progress = viewModel.getNumberOfRooms()
+
+    }
+
+    fun saveUI() {
+        viewModel.saveSpinnerPosition(spinner_listing_type.selectedItemPosition)
+        viewModel.saveNumberOfRooms(seekbar_total_rooms.progress)
+        viewModel.saveNumberOfBedroom(seekbar_bedrooms.progress)
     }
 
 }
