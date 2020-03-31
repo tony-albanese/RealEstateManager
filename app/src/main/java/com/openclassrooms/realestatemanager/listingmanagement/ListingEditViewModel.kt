4a39658a80773b5b2f4610dd28fedc0d14abb4bc
tony-albanese <tony.albanese2000@gmail.com>
2020-03-31 13:39:29 +0200

    Added application parameter to constructor and initialized ListingRepository object.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -1,7 +1,10 @@
 package com.openclassrooms.realestatemanager.listingmanagement
 
+import android.app.Application
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.openclassrooms.realestatemanager.database_files.AppDatabase
+import com.openclassrooms.realestatemanager.database_files.ListingRepository
 
 /*
 This class is designed to preserve the UI state when editing
@@ -9,13 +12,17 @@ a listing.
  */
 
 class ListingEditViewModel(
+        private val application: Application,
         private val handle: SavedStateHandle
 ) : ViewModel() {
 
     var address: String
+    val repository: ListingRepository
 
     init {
         address = handle["tv_listing_street_address"] ?: ""
+        val listingDao = AppDatabase.getDatabase(application).listingDao()
+        repository = ListingRepository(listingDao)
     }
 
 }
