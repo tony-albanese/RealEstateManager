dc982514632179aafd715e7c9abc47d806829cd2
tony-albanese <tony.albanese2000@gmail.com>
2020-04-02 16:33:23 +0200

    Created method to launch activity (as a callback) when the user hits OK after trying to exit the EditListing activity.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
@@ -1,10 +1,14 @@
 package com.openclassrooms.realestatemanager.Utilities
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 
 class CustomDialogBuilder(
-        val context: Context
+        val context: Context,
+        val launchClass: Class<out Activity>
 ) {
 
     val builder: AlertDialog.Builder
@@ -18,9 +22,18 @@ class CustomDialogBuilder(
         return builder.setTitle("Warning")
                 .setMessage("Are you sure? Unsaved changes will be discarded")
                 .setNegativeButton("Cancel", null)
-                .setPositiveButton("Yes", null)
-
+                .setPositiveButton("Yes", createPositiveButtonListener())
     }
 
+    /*
+    Method will create the listener that will run when the user selects ok after hitting the back
+    button or cancel.
+     */
 
+    fun createPositiveButtonListener(): DialogInterface.OnClickListener {
+        return DialogInterface.OnClickListener { dialogInterface, i ->
+            val intent = Intent(context, launchClass)
+            context.startActivity(intent)
+        }
+    }
 }
