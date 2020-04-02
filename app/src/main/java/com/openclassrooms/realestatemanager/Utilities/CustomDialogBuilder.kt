999c3b2e18cf2f63ce6290619ac96de70c894844
tony-albanese <tony.albanese2000@gmail.com>
2020-04-02 15:14:11 +0200

    Created the dialog but not the listeners for positive button.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
@@ -1,9 +1,26 @@
 package com.openclassrooms.realestatemanager.Utilities
 
+import android.app.AlertDialog
 import android.content.Context
 
 class CustomDialogBuilder(
         val context: Context
 ) {
-    
+
+    val builder: AlertDialog.Builder
+
+    init {
+        builder = AlertDialog.Builder(context)
+    }
+
+    fun buildWarningDialog(): AlertDialog.Builder {
+        
+        return builder.setTitle("Warning")
+                .setMessage("Are you sure? Unsaved changes will be discarded")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Yes", null)
+
+    }
+
+
 }
