c0f3fff0f0577cf38bf261260b507eb4602f0ee8
tony-albanese <tony.albanese2000@gmail.com>
2020-04-03 11:51:50 +0200

    Removed method to create click listener. It will be passed as a parameter from the parent class instead.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
@@ -3,7 +3,6 @@ package com.openclassrooms.realestatemanager.Utilities
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
-import android.content.Intent
 
 class CustomDialogBuilder(
         val context: Context
@@ -21,18 +20,7 @@ class CustomDialogBuilder(
                 .setNegativeButton("Cancel", null)
                 .setPositiveButton("Yes", onYesButtonClickListener)
     }
-
-    /*
-    Method will create the listener that will run when the user selects ok after hitting the back
-    button or cancel.
-     */
-    fun createPositiveButtonListener(): DialogInterface.OnClickListener {
-        return DialogInterface.OnClickListener { dialogInterface, i ->
-            val intent = Intent(context, launchClass)
-            context.startActivity(intent)
-        }
-    }
-
+    
     fun createSaveListingDialog(onYesButtonClickListener: DialogInterface.OnClickListener): AlertDialog.Builder {
         return builder.setTitle("Save Listing")
                 .setMessage("Save the current listing?")
