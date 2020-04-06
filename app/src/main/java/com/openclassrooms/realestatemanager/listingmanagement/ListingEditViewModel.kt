b5966f650b0fdebb2add4ea1ed1a7c1bf0504a58
tony-albanese <tony.albanese2000@gmail.com>
2020-04-06 12:27:43 +0200

    Starting to initialize listing for exposure to outside classes.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.openclassrooms.realestatemanager.database_files.AppDatabase
+import com.openclassrooms.realestatemanager.database_files.Listing
 import com.openclassrooms.realestatemanager.database_files.ListingRepository
 
 /*
@@ -21,9 +22,32 @@ class ListingEditViewModel(
     val testBindingVariable: String = "This is a test value."
     val repository: ListingRepository
 
+    val listing: Listing //This is the member variable that will be exposed to the outside world.
+
     init {
         val listingDao = AppDatabase.getDatabase(application).listingDao()
         repository = ListingRepository(listingDao)
+
+        when (listingId) {
+            0.toLong() -> {
+                listing = createNewListing()
+            }
+            else -> {
+                listing = createNewListing().apply {
+                    
+                }
+            }
+        }
     }
-    
+
+    fun getCurrentListing() {
+
+    }
+
+    fun createNewListing(): Listing {
+        return Listing().apply {
+
+        }
+    }
+
 }
