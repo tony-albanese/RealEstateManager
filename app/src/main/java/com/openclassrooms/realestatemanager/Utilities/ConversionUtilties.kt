ca818cdaaaf64f4d773546735778fa53b291419b
tony-albanese <tony.albanese2000@gmail.com>
2020-04-06 22:00:30 +0200

    Wrote method to convert integer to USD format.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/ConversionUtilties.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/ConversionUtilties.kt
@@ -1,6 +1,19 @@
 package com.openclassrooms.realestatemanager.Utilities
 
+import java.text.NumberFormat
+import java.util.*
+
 class ConversionUtilities {
+
+    fun formatCurrencyIntToString(amount: Int, locale: Locale): String {
+        val value = locale.let { locale: Locale ->
+            NumberFormat.getCurrencyInstance(locale).apply {
+                this.maximumFractionDigits = 0
+                currency = Currency.getInstance("USD")
+            }
+        }
+        return value.format(amount.toLong())
+    }
 
     
 
