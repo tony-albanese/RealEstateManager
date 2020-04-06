fe94b50b63746ba968cea9264dea2d0d28615873
tony-albanese <tony.albanese2000@gmail.com>
2020-04-06 22:01:22 +0200

    Wrote unit test to check conversion of integer input to USD string.

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/UtiltiesTests.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/UtiltiesTests.kt
@@ -1,9 +1,29 @@
 package com.openclassrooms.realestatemanager
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.openclassrooms.realestatemanager.Utilities.ConversionUtilities
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 class UtiltiesTests {
-    
+    private lateinit var locale: Locale
+
+    @Before
+    fun initializeVariables() {
+        locale = Locale("EN", "US")
+    }
+
+    @Test
+    fun testIntToStringConversion() {
+        val conversionUtilities = ConversionUtilities()
+        val expectedAmount: String = "$1,000"
+
+        val convertedAmount = conversionUtilities.formatCurrencyIntToString(1000, locale)
+        val isEqual = expectedAmount.compareTo(convertedAmount) == 0
+        assertEquals(expectedAmount, convertedAmount)
+    }
 }
