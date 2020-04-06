8e8ec29869b8af8ec35ca137b1160ccb005ac6d6
tony-albanese <tony.albanese2000@gmail.com>
2020-04-06 12:19:44 +0200

    Initialized repository.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -3,6 +3,8 @@ package com.openclassrooms.realestatemanager.listingmanagement
 import android.app.Application
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.openclassrooms.realestatemanager.database_files.AppDatabase
+import com.openclassrooms.realestatemanager.database_files.ListingRepository
 
 /*
 This class is designed to preserve the UI state when editing
@@ -14,5 +16,14 @@ class ListingEditViewModel(
         private val handle: SavedStateHandle,
         val listingId: Long
 ) : ViewModel() {
+
+
     val testBindingVariable: String = "This is a test value."
+    val repository: ListingRepository
+
+    init {
+        val listingDao = AppDatabase.getDatabase(application).listingDao()
+        repository = ListingRepository(listingDao)
+    }
+    
 }
