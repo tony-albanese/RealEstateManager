25e55149f12c45b0d8b3fe3a73a0aa3dd3bc1de6
tony-albanese <tony.albanese2000@gmail.com>
2020-04-09 12:27:33 +0200

    Changed TAG values for easier debugging.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/ListingPriceBindingAdapters.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/ListingPriceBindingAdapters.kt
@@ -1,21 +1,21 @@
 package com.openclassrooms.realestatemanager.Utilities
 
 import android.util.Log
-import android.view.View
 import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
 import androidx.databinding.InverseBindingListener
 import androidx.databinding.InverseMethod
+import java.util.*
 
 object ListingPriceBindingAdapters {
 
-    val TAG: String = "PriceConversion"
+    val TAG: String = "DEBUG"
 
     @BindingAdapter("listingPrice")
     @JvmStatic
     fun setListingPrice(et: EditText, value: String) {
-        Log.i(TAG, "Binding adapter int to String called")
+        Log.i(TAG, "Binding adapter setListingPrice() called")
         if (et.text.toString() != value) {
             et.setText(value)
         }
@@ -25,7 +25,7 @@ object ListingPriceBindingAdapters {
     @InverseBindingAdapter(attribute = "listingPrice")
     @JvmStatic
     fun getListingPrice(et: EditText): String {
-        Log.i(TAG, "Binding adpater string to int called")
+        Log.i(TAG, "Binding adpater getListingPrice() called")
         val etText = et.text.toString()
         return etText
     }
@@ -37,38 +37,24 @@ object ListingPriceBindingAdapters {
             et: EditText,
             attrChange: InverseBindingListener
     ) {
-        Log.i(TAG, "setListener called")
-        et.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) {
-
-            } else {
-                if (et.text.toString().isNullOrBlank() || et.text.toString().isBlank()) {
-                    et.setText("$0")
-                }
-                val currentText = et.text.toString()
-                Log.i(TAG, "Current text: " + currentText)
-                //TODO This is where the string needs to be formatted and set.
-                et.setText(currentText + "$$$")
-                attrChange.onChange()
-            }
-
-        }
+        Log.i("DEBUG", "Binding Adapter setListener() called")
+        attrChange.onChange()
     }
 }
 
 object ListingPriceConverters {
 
-    val TAG: String = "PriceConversion"
+    val TAG: String = "DEBUG"
     @InverseMethod("priceStringToInteger")
     @JvmStatic
-    fun intToPriceString(value: Int): String {
-        Log.i(TAG, "intToPriceString called")
-        return "$ " + value.toString()
+    fun intToPriceString(locale: Locale, value: Int): String {
+        Log.i(TAG, "Method intToPriceString() called")
+        return ConversionUtilities.formatCurrencyIntToString(value, locale)
     }
 
     @JvmStatic
-    fun priceStringToInteger(value: String): Int {
-        Log.i(TAG, "priceStringToInteger called")
+    fun priceStringToInteger(locale: Locale, value: String): Int {
+        Log.i(TAG, "Inverse method priceStringToInteger() called")
         if (value.isNullOrEmpty()) {
             return 0
         }
