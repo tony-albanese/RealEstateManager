bb4c8ea9aac2af8c779396cb7afde374231d84bc
tony-albanese <tony.albanese2000@gmail.com>
2020-04-09 13:53:05 +0200

    Added methods to convert int to string and vice versa for the area edit text.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/ListingBathroomBindingAdapters.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/ListingBathroomBindingAdapters.kt
@@ -14,4 +14,22 @@ object ListingDataTypeConverters {
     fun bathroomsToProgress(numberOfBathrooms: Double): Int {
         return (numberOfBathrooms * 2).toInt()
     }
+
+    @InverseMethod("areaIntToString")
+    @JvmStatic
+    fun areaStringToInt(areaString: String): Int {
+        if (areaString.isNullOrBlank() || areaString.isEmpty()) {
+            return 0
+        } else {
+            val filteredString = areaString.filter {
+                it.isDigit()
+            }
+            return filteredString.toInt()
+        }
+    }
+
+    @JvmStatic
+    fun areaIntToString(value: Int): String {
+        return value.toString()
+    }
 }
