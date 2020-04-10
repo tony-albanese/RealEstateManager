6d1bb8a9c2c7175d093ac7ca19ad9cf9ab003c7b
tony-albanese <tony.albanese2000@gmail.com>
2020-04-10 10:20:13 +0200

    Rewrote suspend function with Dispaters.IO - move coroutine to a background thread.

--- app/src/main/java/com/openclassrooms/realestatemanager/database_files/ListingRepository.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/ListingRepository.kt
@@ -1,12 +1,15 @@
 package com.openclassrooms.realestatemanager.database_files
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class ListingRepository(
         private val listingDao: ListingDao
 ) {
     val allListings = listingDao.getAllListings()
 
-    suspend fun insert(listing: Listing): Long {
-        return listingDao.insertListing(listing)
+    suspend fun insert(listing: Listing): Long = withContext(Dispatchers.IO) {
+        listingDao.insertListing(listing)
     }
 
     suspend fun delete() {
