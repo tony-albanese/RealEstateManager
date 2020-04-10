7e1800a2c3ed5831af456b1deddd1b8d70666631
tony-albanese <tony.albanese2000@gmail.com>
2020-04-10 10:26:34 +0200

    Added error check on the return value after saving listing to the database.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -1,14 +1,18 @@
 package com.openclassrooms.realestatemanager.listingmanagement
 
-import android.app.AlertDialog
 import android.app.Application
 import android.content.DialogInterface
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.openclassrooms.realestatemanager.database_files.AppDatabase
 import com.openclassrooms.realestatemanager.database_files.Listing
 import com.openclassrooms.realestatemanager.database_files.ListingRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 /*
 This class is designed to preserve the UI state when editing
@@ -18,7 +22,7 @@ a listing.
 class ListingEditViewModel(
         private val application: Application,
         val listingId: Long
-) : ViewModel() {
+) : ViewModel(), CoroutineScope {
 
     
     val repository: ListingRepository
@@ -41,8 +45,28 @@ class ListingEditViewModel(
         currentListing.value?.listingPrice = newValue
     }
 
-    fun saveListingToDatabase(builder: MaterialAlertDialogBuilder){
-        builder.show()
+    fun saveListingToDatabase(builder: MaterialAlertDialogBuilder) {
+        viewModelScope.launch {
+            val returnedID = repository.insert(currentListing.value!!)
+
+            if (!returnedID.equals(0)) {
+                builder.show()
+            } else {
+                builder.setMessage("Something went wrong.")
+                        .setPositiveButton("OK", createPositiveErrorButtonLisenter())
+                        .show()
+            }
+
+        }
+    }
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
+    private fun createPositiveErrorButtonLisenter(): DialogInterface.OnClickListener {
+        return DialogInterface.OnClickListener { dialogInterface, i ->
+            dialogInterface.dismiss()
+        }
     }
 
 }
