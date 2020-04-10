54c539f4177a1035a1c8820c881577cb2a37ff7a
tony-albanese <tony.albanese2000@gmail.com>
2020-04-10 14:52:47 +0200

    Added method to iterate over the items in the hashmap of views and to check if their input is non-blank.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/FormValidatorUtilities.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/FormValidatorUtilities.kt
@@ -1,11 +1,61 @@
 package com.openclassrooms.realestatemanager.Utilities
 
+import android.graphics.Color
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.get
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 object FormValidatorUtilities {
 
+    fun validateForm(viewMap: HashMap<Int, View>) {
+
+        viewMap.forEach { (i, view) ->
+            when (view) {
+                is TextInputEditText -> {
+                    val materialEditText = view as TextInputEditText
+                    val text = materialEditText?.text?.toString()?.trim() ?: ""
+                    if (text.isBlank()) {
+                        materialEditText.setError("This is blank.")
+                    }
+
+                }
+                is EditText -> {
+                    val editText = view as EditText
+                    val text = editText?.text?.toString()?.trim() ?: ""
+                    if (text.isBlank()) {
+                        editText.setBackgroundColor(Color.RED)
+                    }
+                }
+                is TextView -> {
+                    val textView = view as TextView
+                    val text = textView?.text?.toString()?.trim() ?: ""
+                    if (text.isBlank()) {
+                        textView.setBackgroundColor(Color.RED)
+                    }
+
+                }
+                is TextInputLayout -> {
+                    val layout = view as TextInputLayout
+                    val materialEditText = layout.get(0) as TextInputEditText
+                    val text = materialEditText?.text?.toString()?.trim() ?: ""
+                    if (text.isBlank()) {
+                        materialEditText.setError("This is blank.")
+                    }
+
+                }
+                else -> {
+
+                }
+            }
+        }
+
+
+    }
 
 }
 
