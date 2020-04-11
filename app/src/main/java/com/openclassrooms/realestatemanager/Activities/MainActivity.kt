811f02fcbe20ed2067db94ec34e805cf4c07b364
tony-albanese <tony.albanese2000@gmail.com>
2020-04-11 13:09:13 +0200

    Removed fab - it was used only for testing purposes.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/MainActivity.kt
@@ -5,18 +5,14 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.database_files.AppDatabase
 import kotlinx.android.synthetic.main.listings_activity_layout.*
-import kotlinx.android.synthetic.main.listings_information_layout.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var fab: FloatingActionButton
     companion object {
         var database: AppDatabase? = null
     }
@@ -28,20 +24,12 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.title = title
 
-        fab = findViewById(R.id.fab_test)
-
         database = Room.databaseBuilder(this,
                 AppDatabase::class.java,
                 "listing-db")
                 .build()
 
-        fab.setOnClickListener {
-            if (listing_info_landscape_frame_layout != null) {
-                Toast.makeText(this, "LARGE", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "SMALL", Toast.LENGTH_SHORT).show()
-            }
-        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
