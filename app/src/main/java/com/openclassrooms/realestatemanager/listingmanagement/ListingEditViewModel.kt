7aee13ecb3bcda6ea0bce174c30ba3d5d0134fec
tony-albanese <tony.albanese2000@gmail.com>
2020-04-11 15:37:20 +0200

    Added methods to save, load, and delete listing from/to file. Also added the required object dependencies to the constructor.

--- app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/listingmanagement/ListingEditViewModel.kt
@@ -2,16 +2,23 @@ package com.openclassrooms.realestatemanager.listingmanagement
 
 import android.app.Application
 import android.content.DialogInterface
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.openclassrooms.realestatemanager.Constants.LISTING_SAVE_FILE
+import com.openclassrooms.realestatemanager.Utilities.DateUtilities
 import com.openclassrooms.realestatemanager.database_files.AppDatabase
 import com.openclassrooms.realestatemanager.database_files.Listing
 import com.openclassrooms.realestatemanager.database_files.ListingRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 import java.util.*
 import kotlin.coroutines.CoroutineContext
 
@@ -23,9 +30,12 @@ a listing.
 class ListingEditViewModel(
         private val application: Application,
         val calendar: Calendar,
-        val listingId: Long
+        val listingId: Long,
+        val gson: Gson,
+        val objectInputStream: ObjectInputStream,
+        val objectOutputStream: ObjectOutputStream
 ) : ViewModel(), CoroutineScope {
-    
+
     val repository: ListingRepository
 
     val currentListing: MutableLiveData<Listing>  //This is the member variable that will be exposed to the outside world.
@@ -53,7 +63,6 @@ class ListingEditViewModel(
                         .setPositiveButton("OK", createPositiveErrorButtonLisenter())
                         .show()
             }
-
         }
     }
 
@@ -74,6 +83,30 @@ class ListingEditViewModel(
             }
         } else {
             //TODO: Set the initial dates for listing and selling.
+            //Check if there is an unsaved listing saved on file. If not,
+            //Set the default dates. Otherwise, restore the listing from the file.
+            currentListing.value?.listingSaleDate = DateUtilities.getDateString(calendar)
+            currentListing.value?.listingDate = DateUtilities.getDateString(calendar)
+        }
+    }
+
+    fun saveListingToFile() {
+        val listingString = gson.toJson(this.currentListing.value)
+        objectOutputStream.writeObject(listingString)
+        objectOutputStream.close()
+    }
+
+    fun loadListingFromFile() {
+        val listingJson = objectInputStream.readObject() as String
+        val listingType = object : TypeToken<Listing>() {}.type
+        val listing = gson.fromJson<Listing>(listingJson, listingType)
+        currentListing.value = listing
+    }
+
+    fun deleteListingFile() {
+        val successfuleDeletion = application.applicationContext.deleteFile(LISTING_SAVE_FILE)
+        if (!successfuleDeletion) {
+            Log.e("DATABASE", "Error deleting file.")
         }
     }
 }
