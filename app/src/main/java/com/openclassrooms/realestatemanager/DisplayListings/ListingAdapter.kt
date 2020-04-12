31cffea0c335b56aa11fa0d0d0716cb516d040ea
tony-albanese <tony.albanese2000@gmail.com>
2020-04-12 12:07:40 +0200

    Added outlines of required methods to implement ViewHolder and Adapter methods.

--- app/src/main/java/com/openclassrooms/realestatemanager/DisplayListings/ListingAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/DisplayListings/ListingAdapter.kt
@@ -1,4 +1,24 @@
 package com.openclassrooms.realestatemanager.DisplayListings
 
-class ListingAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListingAdapter : RecyclerView.Adapter<ListingAdapter.ListingViewHolder>() {
+
+    inner class ListingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListingViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: ListingViewHolder, position: Int) {
+      
+    }
 }
