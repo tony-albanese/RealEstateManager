1d8a5d8680946ec9dce435da2ae854d9289d4f59
tony-albanese <tony.albanese2000@gmail.com>
2020-04-18 22:15:04 +0200

    Added view model and added TODOs.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/DisplayListingPortaitActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/DisplayListingPortaitActivity.kt
@@ -2,10 +2,13 @@ package com.openclassrooms.realestatemanager.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.database_files.ListingViewModel
 
 class DisplayListingPortaitActivity : AppCompatActivity() {
 
+    lateinit var listingViewModel: ListingViewModel
     /*
     This is the activity that will be launched if the device is in portrait mode.
     It will display the info for a selected listing.
@@ -13,6 +16,10 @@ class DisplayListingPortaitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //TODO: Inflate the layout to display the listing information.
+        //TODO: Switch the data binding.
         setContentView(R.layout.activity_main)
+
+        listingViewModel = ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(application)).get(ListingViewModel::class.java)
+
     }
 }
