a450620377f2ad1f7374df0d18646e205667ecb9
tony-albanese <tony.albanese2000@gmail.com>
2020-04-18 17:30:39 +0200

    Set selection properties and TODOs.

--- app/src/main/java/com/openclassrooms/realestatemanager/DisplayListings/ListingAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/DisplayListings/ListingAdapter.kt
@@ -1,5 +1,6 @@
 package com.openclassrooms.realestatemanager.DisplayListings
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,11 @@ import java.util.*
 class ListingAdapter(val locale: Locale, val callback: (Listing) -> Unit) : RecyclerView.Adapter<ListingAdapter.ListingViewHolder>() {
 
     private var listings = emptyList<Listing>()
+    var previousView: View? = null
+    var previousPosition = -1
+    var selectedPosition = -1
+    var selectedView: View? = null
+
     inner class ListingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val listingItemType = itemView.findViewById<MaterialTextView>(R.id.tv_listing_item_listing_type)
         val listingItemCity = itemView.findViewById<MaterialTextView>(R.id.tv_listing_item_listing_city)
@@ -36,6 +42,22 @@ class ListingAdapter(val locale: Locale, val callback: (Listing) -> Unit) : Recy
         holder.listingItemPrice.text = ConversionUtilities.formatCurrencyIntToString(currentListing.listingPrice, locale)
 
         holder.itemView.setOnClickListener {
+            //TODO: Set the style instead of the individual properties.
+            //TODO: Clean up the if statement.
+            selectedPosition = position
+            if (selectedPosition == previousPosition) {
+
+            } else if (selectedPosition != previousPosition) {
+                previousPosition = selectedPosition
+                selectedPosition = position
+                previousView = selectedView
+                selectedView = holder.itemView
+                previousView?.setBackgroundColor(Color.TRANSPARENT)
+                selectedView?.setBackgroundColor(Color.BLUE)
+            }
+
+
+
             callback(currentListing)
         }
 
