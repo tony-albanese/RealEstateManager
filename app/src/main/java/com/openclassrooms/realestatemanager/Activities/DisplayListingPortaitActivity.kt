8847653e87617a2e6f12131ef15dda49665cd2c6
tony-albanese <tony.albanese2000@gmail.com>
2020-04-19 14:00:19 +0200

    Retrieved listing id from the intent and used it to retrieve listing from database.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/DisplayListingPortaitActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/DisplayListingPortaitActivity.kt
@@ -18,16 +18,17 @@ class DisplayListingPortaitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
-
         listingViewModel = ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(application)).get(ListingViewModel::class.java)
 
         val binding: ListingInformationDetailLayoutBinding = DataBindingUtil.setContentView(this, R.layout.listing_information_detail_layout)
         binding.viewModel = listingViewModel
         binding.lifecycleOwner = this
 
-        //TODO: Retrieve the id from the intent.
-        // listingViewModel.getListingForPortraitMode(1.toLong())
-
+        val intent = intent
+        intent.getLongExtra("LISTING_ID", 0).let {
+            if (it != 0.toLong()) {
+                listingViewModel.getListingForPortraitMode(it)
+            }
+        }
     }
 }
