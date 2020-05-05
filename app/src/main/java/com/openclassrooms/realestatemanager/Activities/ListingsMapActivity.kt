92a776b53996ac1a17f66be4b18fe024c6eb4112
tony-albanese <tony.albanese2000@gmail.com>
2020-05-05 22:58:34 +0200

    Removed TomTom libraries.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingsMapActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingsMapActivity.kt
@@ -3,12 +3,6 @@ package com.openclassrooms.realestatemanager.Activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.openclassrooms.realestatemanager.R
-import com.tomtom.online.sdk.search.OnlineSearchApi
-import com.tomtom.online.sdk.search.SearchApi
-import com.tomtom.online.sdk.search.api.SearchError
-import com.tomtom.online.sdk.search.api.fuzzy.FuzzySearchResultListener
-import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchQueryBuilder
-import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResponse
 
 class ListingsMapActivity : AppCompatActivity() {
 
