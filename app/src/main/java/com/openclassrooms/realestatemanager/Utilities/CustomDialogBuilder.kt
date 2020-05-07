fabe2da66ab08fa1de5921fc40742c0250840d63
tony-albanese <tony.albanese2000@gmail.com>
2020-05-07 21:30:13 +0200

    Added method to build Listing Location request dialog.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/CustomDialogBuilder.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.openclassrooms.realestatemanager.Activities.ListingsMapActivity
 import com.openclassrooms.realestatemanager.Activities.MainActivity
 
 class CustomDialogBuilder(
@@ -42,5 +43,22 @@ class CustomDialogBuilder(
                     dialogInterface.dismiss()
                 })
 
+    }
+
+    fun buildRequestListingLocationDialog(listingId: Long): MaterialAlertDialogBuilder {
+        return MaterialAlertDialogBuilder(context)
+                .setTitle("Listing Location")
+                .setMessage("Get listing location details?")
+                .setNegativeButton("No", DialogInterface.OnClickListener { dialogInterface, i ->
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                    dialogInterface.dismiss()
+                })
+                .setPositiveButton("OK", DialogInterface.OnClickListener { dialogInterface, i ->
+                    val intent = Intent(context, ListingsMapActivity::class.java)
+                    intent.putExtra(LISTING_ID, listingId)
+                    intent.putExtra(ACTIVITY_TASK, TASK_SELECT_LISTING_LOCATION)
+                    context.startActivity(intent)
+                })
     }
 }
