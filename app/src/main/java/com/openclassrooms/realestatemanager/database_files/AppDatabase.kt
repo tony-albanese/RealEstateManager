d565a48b85ff0194ae915a5a8b2997e20d5bf82d
tony-albanese <tony.albanese2000@gmail.com>
2020-05-07 22:03:45 +0200

    Added @TypeConverters annotation.

--- app/src/main/java/com/openclassrooms/realestatemanager/database_files/AppDatabase.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/AppDatabase.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.openclassrooms.realestatemanager.Utilities.ListingDataTypeConverters
 
 @Database(entities = [Listing::class], version = 1)
+@TypeConverters(ListingDataTypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun listingDao(): ListingDao
 
