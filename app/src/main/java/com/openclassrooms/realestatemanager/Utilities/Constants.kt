6224eb26724c9c4a26a5d9f0110573d1f50bbef7
tony-albanese <tony.albanese2000@gmail.com>
2020-05-07 21:14:03 +0200

    Initial commit.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/Constants.kt
@@ -0,0 +1,9 @@
+package com.openclassrooms.realestatemanager.Utilities
+
+
+val ACTIVITY_TASK: String = "activity_task"
+val TASK_DISPLAY_ALL_LISTINGS: Int = 1000
+val TASK_LOAD_SINGLE_LISTING: Int = 1200
+val TASK_SELECT_LISTING_LOCATION: Int = 1300
+
+val LISTING_ID: String = "listing_id"
