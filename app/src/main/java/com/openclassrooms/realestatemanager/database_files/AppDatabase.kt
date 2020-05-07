99e7399903d630d5653336fdd52841923752d816
tony-albanese <tony.albanese2000@gmail.com>
2020-05-07 22:20:24 +0200

    Fixed wrong class listed as TypeConverter.

--- app/src/main/java/com/openclassrooms/realestatemanager/database_files/AppDatabase.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database_files/AppDatabase.kt
@@ -5,10 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.openclassrooms.realestatemanager.Utilities.ListingDataTypeConverters
+import com.openclassrooms.realestatemanager.Utilities.DatabaseTypeConverter
 
 @Database(entities = [Listing::class], version = 1)
-@TypeConverters(ListingDataTypeConverters::class)
+@TypeConverters(DatabaseTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun listingDao(): ListingDao
 
