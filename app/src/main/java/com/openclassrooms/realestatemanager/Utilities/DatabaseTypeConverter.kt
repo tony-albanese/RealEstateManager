7817b94e527b01652bdc413082ac0e7f734e52ae
tony-albanese <tony.albanese2000@gmail.com>
2020-05-07 22:00:58 +0200

    Added type converter to convert LatLng object to Json string and vice versa.

--- app/src/main/java/com/openclassrooms/realestatemanager/Utilities/DatabaseTypeConverter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utilities/DatabaseTypeConverter.kt
@@ -1,4 +1,19 @@
 package com.openclassrooms.realestatemanager.Utilities
 
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.mapbox.mapboxsdk.geometry.LatLng
+
 class DatabaseTypeConverter {
+    @TypeConverter
+    fun fromLatLng(latLng: LatLng): String {
+        return Gson().toJson(latLng)
+    }
+
+    @TypeConverter
+    fun latLngFromString(latLngJson: String): LatLng {
+        val type = object : TypeToken<LatLng>() {}.type
+        return Gson().fromJson<LatLng>(latLngJson, type)
+    }
 }
