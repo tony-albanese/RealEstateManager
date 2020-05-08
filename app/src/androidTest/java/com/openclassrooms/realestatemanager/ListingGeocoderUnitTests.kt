8467182ca6b7c6e6bab954e0d9db62fe4b463f00
tony-albanese <tony.albanese2000@gmail.com>
2020-05-08 16:09:08 +0200

    Refactored test to pass hashmap of keys in to be more testable.

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/ListingGeocoderUnitTests.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/ListingGeocoderUnitTests.kt
@@ -2,11 +2,14 @@ package com.openclassrooms.realestatemanager
 
 import android.content.Context
 import android.net.Uri
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.openclassrooms.realestatemanager.Geolocation.ListingGeocoder
+import com.openclassrooms.realestatemanager.Utilities.LOCATION_IQ_KEY
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.`when`
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -16,24 +19,29 @@ class ListingGeocoderUnitTests {
     val FAKE_KEY = "FAKEKEY"
 
 
-    lateinit var mockContext: Context
+    @Mock
+    private lateinit var mockContext: Context
+
+    lateinit var context: Context
 
     @Before
     fun setup() {
-        //MockitoAnnotations.initMocks(this)
-        //mockContext = MockContext()
+        context = getInstrumentation().targetContext
+        MockitoAnnotations.initMocks(this)
     }
 
     @Test
     fun testGeocodingBaseUriBuilder() {
 
-        val expectedUrl = ""
-        `when`(mockContext.getString(R.string.locationIQ_token))
-                .thenReturn(FAKE_KEY)
-        val listingGeocoder = ListingGeocoder(uriBuilder, mockContext)
-        val testUrl = listingGeocoder.geocodingBaseUrl
-        System.out.println(testUrl)
-        // assertEquals()
+
+        val expectedUrl = "https://eu1.locationiq.com/v1/search.php?key=FAKEKEY"
+        val map = HashMap<String, String>()
+        map.put(LOCATION_IQ_KEY, FAKE_KEY)
+
+        val listingGeocoder = ListingGeocoder(uriBuilder, context, map)
+
+        val testUrl = listingGeocoder.buildForwardGeocodingBaseUrl(map.get(LOCATION_IQ_KEY)!!)
+        assert(testUrl.equals(expectedUrl))
 
     }
 }
