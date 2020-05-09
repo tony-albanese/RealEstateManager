2bab24dafef5d1b43dd0033957cbc914dd4bd5e6
tony-albanese <tony.albanese2000@gmail.com>
2020-05-09 11:02:24 +0200

    Initialized listing view model object.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingsMapActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingsMapActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
@@ -15,6 +16,7 @@ import com.openclassrooms.realestatemanager.Utilities.LISTING_ID
 import com.openclassrooms.realestatemanager.Utilities.LOCATION_IQ_KEY
 import com.openclassrooms.realestatemanager.Utilities.TASK_SELECT_LISTING_LOCATION
 import com.openclassrooms.realestatemanager.database_files.Listing
+import com.openclassrooms.realestatemanager.database_files.ListingViewModel
 import kotlinx.android.synthetic.main.activity_listings_map.*
 
 class ListingsMapActivity : AppCompatActivity(), ListingGeocoder.OnConnectionResultListener {
@@ -26,6 +28,7 @@ class ListingsMapActivity : AppCompatActivity(), ListingGeocoder.OnConnectionRes
 
     lateinit var listingGeocoder: ListingGeocoder
     lateinit var listing: Listing
+    lateinit var listingViewModel: ListingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +49,8 @@ class ListingsMapActivity : AppCompatActivity(), ListingGeocoder.OnConnectionRes
             listingId = it.getLongExtra(LISTING_ID, 0)
         }
 
-        //TODO: Setup ViewModel
+        listingViewModel = ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(application)).get(ListingViewModel::class.java)
+        
         map_view?.getMapAsync(mapReadyCallback)
     }
 
