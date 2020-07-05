bf07e36440908dfacda3621b47f192a10ef3543e
tony-albanese <tony.albanese2000@gmail.com>
2020-07-05 12:22:23 +0200

    Refactored code to load a base map.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/ListingMapBaseActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/ListingMapBaseActivity.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.database_files.ListingViewModel
-import kotlinx.android.synthetic.main.activity_listings_map.*
 
-class ListingMapBaseActivity : AppCompatActivity() {
+open class ListingMapBaseActivity : AppCompatActivity() {
 
     lateinit var listingViewModel: ListingViewModel
     lateinit var map: MapboxMap
+    lateinit var map_view: MapView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,15 +25,19 @@ class ListingMapBaseActivity : AppCompatActivity() {
         setContentView(R.layout.activity_listings_map)
 
         listingViewModel = ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(application)).get(ListingViewModel::class.java)
-        map_view?.getMapAsync(mapReadyCallback)
+        map_view = findViewById<MapView>(R.id.map_view)
+        inializeActivity()
 
     }
 
-    val mapReadyCallback = object : OnMapReadyCallback {
-        override fun onMapReady(mapboxMap: MapboxMap) {
-            map = mapboxMap
-            mapboxMap.setStyle(Style.MAPBOX_STREETS)
-
+    open fun inializeActivity() {
+        val mapReadyCallback = object : OnMapReadyCallback {
+            override fun onMapReady(mapboxMap: MapboxMap) {
+                map = mapboxMap
+                mapboxMap.setStyle(Style.MAPBOX_STREETS)
+            }
         }
+        map_view.getMapAsync(mapReadyCallback)
     }
+
 }
