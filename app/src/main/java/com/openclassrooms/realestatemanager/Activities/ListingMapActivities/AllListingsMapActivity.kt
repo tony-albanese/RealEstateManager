9e5df68305eeedf5494f759bad9876427ec835a0
tony-albanese <tony.albanese2000@gmail.com>
2020-07-05 17:41:27 +0200

    Added permission check and permission request.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/AllListingsMapActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/AllListingsMapActivity.kt
@@ -3,13 +3,18 @@ package com.openclassrooms.realestatemanager.Activities.ListingMapActivities
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
+import com.openclassrooms.realestatemanager.R
 
+private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
 class AllListingsMapActivity : ListingMapBaseActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         super.inializeActivity()
-
+        requestForegroundPermission()
     }
 
     private fun isForegroundPermissionGranted(): Boolean {
@@ -19,7 +24,30 @@ class AllListingsMapActivity : ListingMapBaseActivity() {
         )
     }
 
-    private fun requestForgroundPermission() {
+    private fun requestForegroundPermission() {
 
+        val provideRationale = isForegroundPermissionGranted()
+        val mapLayout = findViewById<ConstraintLayout>(R.id.map_activity_layout)
+
+        if (provideRationale) {
+            Snackbar.make(
+                    mapLayout,
+                    "You need to grant permission to see the listings near you",
+                    Snackbar.LENGTH_LONG
+            )
+                    .setAction("OK") {
+                        ActivityCompat.requestPermissions(this@AllListingsMapActivity,
+                                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                                REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+                        )
+                    }
+                    .show()
+        } else {
+            ActivityCompat.requestPermissions(
+                    this@AllListingsMapActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+            )
+        }
     }
 }
