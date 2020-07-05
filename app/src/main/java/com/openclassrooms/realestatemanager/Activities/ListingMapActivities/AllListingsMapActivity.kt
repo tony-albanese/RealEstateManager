6868a2c0b05be615f881a84b544adea242446302
tony-albanese <tony.albanese2000@gmail.com>
2020-07-05 16:22:14 +0200

    Initial commit for AllListingsMapActivity.kt and added entry into Manifest.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/AllListingsMapActivity.kt
@@ -0,0 +1,9 @@
+package com.openclassrooms.realestatemanager.Activities.ListingMapActivities
+
+import android.os.Bundle
+
+class AllListingsMapActivity : ListingMapBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
