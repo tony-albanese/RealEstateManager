51dbe92632a7a5f30f3b348a44a6b9f8c9c72d5a
tony-albanese <tony.albanese2000@gmail.com>
2020-07-05 17:21:00 +0200

    Stubbed methods for requesting permissions for location (foreground location).

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/AllListingsMapActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/AllListingsMapActivity.kt
@@ -1,11 +1,25 @@
 package com.openclassrooms.realestatemanager.Activities.ListingMapActivities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class AllListingsMapActivity : ListingMapBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         super.inializeActivity()
+
+    }
+
+    private fun isForegroundPermissionGranted(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    private fun requestForgroundPermission() {
 
     }
 }
