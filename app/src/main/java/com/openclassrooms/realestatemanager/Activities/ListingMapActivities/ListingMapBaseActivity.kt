27742a2c19b8fa469f4eafcdb1e4add52e569a6d
tony-albanese <tony.albanese2000@gmail.com>
2020-07-05 12:35:19 +0200

    Added member variables for keyMap and incomingIntent object.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/ListingMapBaseActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingMapActivities/ListingMapBaseActivity.kt
@@ -11,13 +11,20 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import com.openclassrooms.realestatemanager.Activities.MainActivity
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.Utilities.LOCATION_IQ_KEY
+import com.openclassrooms.realestatemanager.database_files.Listing
 import com.openclassrooms.realestatemanager.database_files.ListingViewModel
 
 open class ListingMapBaseActivity : AppCompatActivity() {
 
     lateinit var listingViewModel: ListingViewModel
+    lateinit var listing: Listing
     lateinit var map: MapboxMap
     lateinit var map_view: MapView
+    var listingId: Long = 0
+    val keyMap = HashMap<String, String>()
+
+    var incomingIntent: Intent? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +33,8 @@ open class ListingMapBaseActivity : AppCompatActivity() {
         Mapbox.getInstance(this, getString(R.string.mapquest_token))
         setContentView(R.layout.activity_listings_map)
 
+        keyMap.put(LOCATION_IQ_KEY, getString(R.string.locationIQ_token))
+        listing = Listing()
         listingViewModel = ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(application)).get(ListingViewModel::class.java)
         map_view = findViewById<MapView>(R.id.map_view)
         inializeActivity()
