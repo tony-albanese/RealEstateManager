417a434b3d1771e5252b990cbd599ba784f46952
tony-albanese <tony.albanese2000@gmail.com>
2020-07-07 11:37:30 +0200

    Added stubs of methods for the adapter.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoAdapter.kt
@@ -1,6 +1,29 @@
 package com.openclassrooms.realestatemanager.ListingPhotos
 
-class ListingPhotoAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListingPhotoAdapter : RecyclerView.Adapter<ListingPhotoAdapter.PhotoViewHolder>() {
+
+
+    inner class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+
+        
+    }
 
 
 }
