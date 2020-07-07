aed1971d47b05506786d29606bbd070dfabcc41a
tony-albanese <tony.albanese2000@gmail.com>
2020-07-07 13:50:27 +0200

    Added member variables for a ListingPhoto object.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhoto.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhoto.kt
@@ -1,4 +1,16 @@
 package com.openclassrooms.realestatemanager.ListingPhotos
 
-class ListingPhoto {
+import android.net.Uri
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "table_listing_photos")
+data class ListingPhoto(
+        @ColumnInfo(name = "photo_id") @PrimaryKey(autoGenerate = true) var photoId: Long = 0,
+        @ColumnInfo(name = "listing_id") var listingId: Long = 0,
+        @ColumnInfo(name = "photo_description") var photoDescription: String = "",
+        @ColumnInfo(name = "photo_uri") var photoUri: Uri? = null
+) {
+
 }
