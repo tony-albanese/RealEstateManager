d61426d9ed1bf740b5223dac1caeee8753c8137c
tony-albanese <tony.albanese2000@gmail.com>
2020-07-07 11:47:02 +0200

    Created ViewHolder.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoAdapter.kt
@@ -1,28 +1,29 @@
 package com.openclassrooms.realestatemanager.ListingPhotos
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.R
 
 class ListingPhotoAdapter : RecyclerView.Adapter<ListingPhotoAdapter.PhotoViewHolder>() {
 
 
     inner class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.listing_photo_item_layout, parent, false)
+        return PhotoViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-
+        return 1
     }
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
 
-        
     }
 
 
