1f67c19aa129bfbc35caee0fa63f8d9825d6e3b6
tony-albanese <tony.albanese2000@gmail.com>
2020-07-07 14:14:17 +0200

    Started to implement the launch intent method to use the devices camera to take a photo.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoUtilities.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoUtilities.kt
@@ -1,4 +1,21 @@
 package com.openclassrooms.realestatemanager.ListingPhotos
 
-class ListingPhotoUtilities {
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.provider.MediaStore
+
+class ListingPhotoUtilities(val context: Context) {
+
+    val REQUEST_IMAGE_CAPTURE = 1253
+
+    fun sendTakePictureIntent(activity: Activity) {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { pictureIntent: Intent ->
+            pictureIntent.resolveActivity(context.packageManager)?.also {
+                activity.startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+
+        }
+    }
 }
+
