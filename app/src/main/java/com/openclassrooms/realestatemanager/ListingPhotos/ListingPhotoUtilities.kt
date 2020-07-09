dd94d50c76e245bb149f43444723ebc59dc6db6e
tony-albanese <tony.albanese2000@gmail.com>
2020-07-09 09:04:40 +0200

    Changed storage directory to devices interal storage.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoUtilities.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoUtilities.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.core.content.FileProvider
@@ -51,7 +50,8 @@ class ListingPhotoUtilities(val context: Context) {
 
         // Create an image file name
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        //val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val storageDir: File? = context.filesDir
         return File.createTempFile(
                 "JPEG_${timeStamp}_", /* prefix */
                 ".jpg", /* suffix */
