9928df1de308f0a9ca8d0f6fe44304adf84d888a
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-16 13:59:17 +0200

    Stubbing methods to handle the process of saving the photos to the database.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoViewModel.kt
@@ -11,8 +11,8 @@ class ListingPhotoViewModel(val application: Application) : ViewModel(
 ) {
     val repository: ListingPhotoRepository
 
-    private val _listingPhotos = MutableLiveData<ListingPhoto>()
-    val listingPhotos: LiveData<ListingPhoto>
+    private val _listingPhotos = MutableLiveData<List<ListingPhoto>>()
+    val listingPhotos: LiveData<List<ListingPhoto>>
         get() = _listingPhotos
 
     init {
@@ -24,6 +24,12 @@ class ListingPhotoViewModel(val application: Application) : ViewModel(
     fun saveListingPhoto(photo: ListingPhoto) {
         //repository.insertPhoto(photo)
     }
+
+    fun getPhotosForLisiting(listingId: Long) {
+        //_listingPhotos.postValue()
+    }
+
+
     //TODO () Need a method to save a ListingPhoto object.
     //TODO () Need a method to fetch all of the listing photo objects for a particular listing (by Id)
     //TODO () Expose all of the listing photos for a listing via LiveData.
