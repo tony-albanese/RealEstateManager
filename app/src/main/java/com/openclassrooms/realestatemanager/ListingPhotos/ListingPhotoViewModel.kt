1870676e966eff5ccb0f10d11a3f8ae127f9ca2a
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-16 13:53:08 +0200

    Added member variable for photos for a particular listing.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoViewModel.kt
@@ -1,6 +1,8 @@
 package com.openclassrooms.realestatemanager.ListingPhotos
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.openclassrooms.realestatemanager.database_files.AppDatabase
 
@@ -9,11 +11,19 @@ class ListingPhotoViewModel(val application: Application) : ViewModel(
 ) {
     val repository: ListingPhotoRepository
 
+    private val _listingPhotos = MutableLiveData<ListingPhoto>()
+    val listingPhotos: LiveData<ListingPhoto>
+        get() = _listingPhotos
+
     init {
         val listingPhotoDao = AppDatabase.getDatabase(application).listingPhotoDao()
         repository = ListingPhotoRepository(listingPhotoDao)
     }
 
+
+    fun saveListingPhoto(photo: ListingPhoto) {
+        //repository.insertPhoto(photo)
+    }
     //TODO () Need a method to save a ListingPhoto object.
     //TODO () Need a method to fetch all of the listing photo objects for a particular listing (by Id)
     //TODO () Expose all of the listing photos for a listing via LiveData.
