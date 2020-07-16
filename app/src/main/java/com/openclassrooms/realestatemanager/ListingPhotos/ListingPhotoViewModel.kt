3560c978b76426fc2fca8e3c8340310d0cec618c
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-16 23:32:45 +0200

    Removed TODOs.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoViewModel.kt
@@ -36,7 +36,4 @@ class ListingPhotoViewModel(val application: Application) : ViewModel(
         get() = Dispatchers.Main
 
 
-    //TODO () Need a method to save a ListingPhoto object.
-    //TODO () Need a method to fetch all of the listing photo objects for a particular listing (by Id)
-    //TODO () Expose all of the listing photos for a listing via LiveData.
 }
