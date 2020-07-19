5697c7920e94dcaa0efe4dc27cef68f2772fe50a
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-19 19:45:25 +0200

    Deleted Google's method to resize bitmap by hand because the Glide library already handles that.

--- app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ListingPhotos/ListingPhotoAdapter.kt
@@ -1,8 +1,6 @@
 package com.openclassrooms.realestatemanager.ListingPhotos
 
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,43 +33,5 @@ class ListingPhotoAdapter(val context: Context, var photoList: ArrayList<Listing
                 .placeholder(context.resources.getDrawable(R.drawable.placeholder_image))
                 .error(context.resources.getDrawable(R.drawable.placeholder_image))
                 .into(holder.imageView)
-
-    }
-
-    //TODO: Fix this method.
-    private fun setPic(imageView: ImageView, photoUri: Uri?) {
-        // Get the dimensions of the View
-        val targetW: Int = imageView.width
-        val targetH: Int = imageView.height
-
-        val bmOptions = BitmapFactory.Options().apply {
-            // Get the dimensions of the bitmap
-            inJustDecodeBounds = true
-
-            val photoW: Int = outWidth
-            val photoH: Int = outHeight
-
-            // Determine how much to scale down the image
-            try {
-                val scaleFactor: Int = Math.min(photoW / targetW, photoH / targetH)
-                inSampleSize = scaleFactor
-            } catch (e: ArithmeticException) {
-                val scaleFactor: Int = Math.min(photoW / 150, photoH / 150)
-                inSampleSize = scaleFactor
-            }
-            
-            // Decode the image file into a Bitmap sized to fill the View
-            inJustDecodeBounds = false
-            inPurgeable = true
-        }
-        BitmapFactory.decodeFile(photoUri?.path, bmOptions)?.also { bitmap ->
-            Glide.with(context)
-                    .load(bitmap)
-                    .placeholder(context.resources.getDrawable(R.drawable.placeholder_image))
-                    .error(context.resources.getDrawable(R.drawable.placeholder_image))
-                    .into(imageView)
-
-            //imageView.setImageBitmap(bitmap)
-        }
     }
 }
