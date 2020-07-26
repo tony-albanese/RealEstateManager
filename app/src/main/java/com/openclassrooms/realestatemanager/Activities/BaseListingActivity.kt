2ee2f3993bb2222c95013171cb53c0b08be23e63
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-26 18:43:31 +0200

    Setup Room database.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/BaseListingActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/BaseListingActivity.kt
@@ -3,11 +3,26 @@ package com.openclassrooms.realestatemanager.Activities
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.database_files.AppDatabase
 
 class BaseListingActivity : AppCompatActivity() {
+
+    companion object {
+        var database: AppDatabase? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.listings_activity_layout)
+
+
+        //Initialize the Room database.
+        MainActivity.database = Room.databaseBuilder(this,
+                AppDatabase::class.java,
+                "listing-db")
+                .build()
+
     }
 }
