23d1b33e4c674d5e6f64fca3d9f620a7aef02878
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-26 19:50:14 +0200

    Setup recycler view. It could be null so it will be setup in a null safe way.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingBaseActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/ListingBaseActivity.kt
@@ -4,23 +4,50 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.DisplayListings.ListingAdapter
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.database_files.ListingViewModel
 import com.openclassrooms.realestatemanager.databinding.ListingsActivityLayoutBinding
+import kotlinx.android.synthetic.main.listings_information_layout.*
+import java.util.*
 
 class ListingBaseActivity : AppCompatActivity() {
 
     //Declare objects that are needed to display the listings.
     lateinit var listingViewModel: ListingViewModel
+    var recyclerView: RecyclerView? = null
+    lateinit var adapter: ListingAdapter
+
+
+    //Misc data about state
+    var landscapeMode: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         listingViewModel = ViewModelProvider(viewModelStore, ViewModelProvider.AndroidViewModelFactory(application)).get(ListingViewModel::class.java)
+
         //Inflate the layout first.
         val binding: ListingsActivityLayoutBinding = DataBindingUtil.setContentView(this, R.layout.listings_activity_layout)
         binding.lifecycleOwner = this
         binding.listingViewModel = listingViewModel
 
+        //Determine if we're in landscape mode.
+        landscapeMode = listing_info_landscape_frame_layout != null
+
+        //Setup the recyclerview and adapter for the listings.
+        adapter = ListingAdapter(Locale("EN", "US"), landscapeMode, globalVariables, itemViewOnClickListenerCallback)
+
+        //This recyclerview might be null.
+        recyclerView = findViewById(R.id.rv_listings)
+        recyclerView?.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            val itemDecor = DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
+            addItemDecoration(itemDecor)
+            adapter = adapter
+        }
     }
 }
