7bb76044ea572f8422a77b8b5a62e970889e5754
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-26 18:50:46 +0200

    Declared and sorted variables needed to display listing photos and the listings themselves.

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/BaseListingActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/BaseListingActivity.kt
@@ -3,11 +3,41 @@ package com.openclassrooms.realestatemanager.Activities
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.openclassrooms.realestatemanager.DisplayListings.ListingAdapter
+import com.openclassrooms.realestatemanager.ListingPhotos.*
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.Utilities.HelperMethods
 import com.openclassrooms.realestatemanager.database_files.AppDatabase
+import com.openclassrooms.realestatemanager.database_files.Listing
+import com.openclassrooms.realestatemanager.database_files.ListingViewModel
+import java.io.File
 
 class BaseListingActivity : AppCompatActivity() {
+
+
+    //Declare references to objects needed to display listing photos.
+    lateinit var photoUtilities: ListingPhotoUtilities
+    lateinit var listingPhotoViewModel: ListingPhotoViewModel
+    var photoRecyclerView: RecyclerView? = null
+    lateinit var photoAdapter: ListingPhotoAdapter
+    var photos: ArrayList<ListingPhoto> = ArrayList<ListingPhoto>()
+    var imageFile: File? = null
+
+    //Declare references to objects related to the selected listing.
+    lateinit var listingViewModel: ListingViewModel
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: ListingAdapter
+    var unpublishedListings = listOf<Listing>()
+
+    //Declare references to utility objects.
+    lateinit var helper: HelperMethods
+    lateinit var globalVariables: GlobalVariableApplication
+
+
+    var landscapeMode: Boolean = false
+
 
     companion object {
         var database: AppDatabase? = null
@@ -17,12 +47,12 @@ class BaseListingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.listings_activity_layout)
 
-
         //Initialize the Room database.
         MainActivity.database = Room.databaseBuilder(this,
                 AppDatabase::class.java,
                 "listing-db")
                 .build()
 
+        
     }
 }
