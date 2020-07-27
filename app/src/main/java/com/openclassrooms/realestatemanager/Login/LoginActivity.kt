ef2c84a03e181f8b2f06b641a649c32725985404
Tony Albanese <tony.albanese2000@gmail.com>
2020-07-27 21:18:43 +0200

    Copied resource files, strings, styles, drawings and layouts from AppSnippet for this design. Added activity for login and set it as the launcher in the manifest.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.openclassrooms.realestatemanager.Login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.openclassrooms.realestatemanager.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_layout)
+    }
+}
