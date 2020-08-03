b267d60b072bb012a7eaa56f8a14884904b6043c
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 16:27:23 +0200

    Added Error class to represent a response that is a general error.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginResponse.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginResponse.kt
@@ -1,4 +1,8 @@
 package com.openclassrooms.realestatemanager.Login
 
-class LoginResponse {
+sealed class LoginResponse<out T : Any?> {
+
+    data class Success<out T : Any>(val data: T) : LoginResponse<T>()
+    data class ExceptionError(val exception: Exception) : LoginResponse<Nothing>()
+    data class Error(val message: String) : LoginResponse<Nothing>()
 }
