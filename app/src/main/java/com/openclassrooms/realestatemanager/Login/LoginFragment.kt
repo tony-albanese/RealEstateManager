c1a51c82e6b2cc0a459e30c3c49a6b1340b86bec
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 10:44:55 +0200

    Set on click listener for text view to take user to the RegistrationFragment.kt when they click on sign-up.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.openclassrooms.realestatemanager.R
 
 class LoginFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.login_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val signUpTextView: TextView? = view.findViewById(R.id.tv_sign_up)
+        signUpTextView?.setOnClickListener {
+            findNavController().navigate(R.id.registrationFragment_dest, null)
+        }
     }
 }
