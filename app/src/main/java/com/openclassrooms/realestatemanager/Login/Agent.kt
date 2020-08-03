c7c1252ff0ac145f5cd90640bd0b93006c4e9ecf
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 13:56:15 +0200

    Changed agentId type to UUID.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/Agent.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/Agent.kt
@@ -3,11 +3,12 @@ package com.openclassrooms.realestatemanager.Login
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "table_agents")
 data class Agent(
         @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
-        @ColumnInfo(name = "agentId") var agentId: Long = 0,
+        @ColumnInfo(name = "agentId") var uuid: UUID = UUID.fromString("0"),
         @ColumnInfo(name = "agentFirstName") var agentFirstName: String = "",
         @ColumnInfo(name = "agentLastName") var agentLastName: String = "",
         @ColumnInfo(name = "agentEmail") var agentEmail: String = "",
