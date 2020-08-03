2b556ce829b464f05180e140cde088b4d9f1af67
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 12:23:21 +0200

    Added method declarations to insert, update an Agent and a method to get an agent by e-mail.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginDao.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginDao.kt
@@ -1,7 +1,15 @@
 package com.openclassrooms.realestatemanager.Login
 
-import androidx.room.Dao
+import androidx.room.*
 
 @Dao
 interface LoginDao {
+    @Query("SELECT * FROM table_agents WHERE agentEmail = :email")
+    fun getAgentByEmail(email: String): Agent
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun registerAgent(agent: Agent): Long
+
+    @Update
+    fun updateAgentInfor(agent: Agent): Long
 }
