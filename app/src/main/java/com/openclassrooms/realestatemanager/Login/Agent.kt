364a17cb547d55d27506da58954dd8f8937bad56
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 11:41:55 +0200

    Added columns for an Agent object.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/Agent.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/Agent.kt
@@ -1,4 +1,17 @@
 package com.openclassrooms.realestatemanager.Login
 
-class Agent {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "table_agents")
+data class Agent(
+        @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
+        @ColumnInfo(name = "agentId") var agentId: Long = 0,
+        @ColumnInfo(name = "agentFirstName") var agentFirstName: String = "",
+        @ColumnInfo(name = "agentLastName") var agentLastName: String = "",
+        @ColumnInfo(name = "agentEmail") var agentEmail: String = "",
+        @ColumnInfo(name = "password") var password: String = "",
+        @ColumnInfo(name = "password_hint") var passwordHint: String = ""
+) {
 }
