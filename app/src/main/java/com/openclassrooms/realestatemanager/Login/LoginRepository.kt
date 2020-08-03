8b71e198eb85462285dd69ab273f282290cad6e5
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 12:23:46 +0200

    Started declaring methods needed to handle login flow.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginRepository.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginRepository.kt
@@ -1,4 +1,14 @@
 package com.openclassrooms.realestatemanager.Login
 
-class LoginRepository {
+class LoginRepository(
+        val loginDao: LoginDao
+) {
+
+    suspend fun registerAgent(agent: Agent) {
+        loginDao.registerAgent(agent)
+    }
+
+    suspend fun getAgentByEmail(email: String): Agent {
+        return loginDao.getAgentByEmail(email)
+    }
 }
