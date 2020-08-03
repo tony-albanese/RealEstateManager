17b55c2c4048e897615caf547a4187f876ef8c95
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 13:53:31 +0200

    Changed type to email.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/RegistrationRequest.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/RegistrationRequest.kt
@@ -3,7 +3,8 @@ package com.openclassrooms.realestatemanager.Login
 data class RegistrationRequest(
         val firstName: String,
         val lastName: String,
+        val email: EmailString.Email,
         val password: String,
-        val passwordHint: String
+        val passwordHint: String = ""
 ) {
 }
