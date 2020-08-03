5bb3ed856e6640d178b666760dc0677e6610ce95
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 14:07:26 +0200

    Made fields nullable and chaned email type from String to Email.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/Agent.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/Agent.kt
@@ -9,10 +9,10 @@ import java.util.*
 data class Agent(
         @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
         @ColumnInfo(name = "agentId") var uuid: UUID = UUID.fromString("0"),
-        @ColumnInfo(name = "agentFirstName") var agentFirstName: String = "",
-        @ColumnInfo(name = "agentLastName") var agentLastName: String = "",
-        @ColumnInfo(name = "agentEmail") var agentEmail: String = "",
-        @ColumnInfo(name = "password") var password: String = "",
+        @ColumnInfo(name = "agentFirstName") var agentFirstName: String? = "",
+        @ColumnInfo(name = "agentLastName") var agentLastName: String? = "",
+        @ColumnInfo(name = "agentEmail") var agentEmail: EmailString.Email? = null,
+        @ColumnInfo(name = "password") var password: String? = "",
         @ColumnInfo(name = "password_hint") var passwordHint: String = ""
 ) {
 }
