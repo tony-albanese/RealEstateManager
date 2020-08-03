5f6f44f314954c8834f8e05f7711b1af86b9e490
Tony Albanese <tony.albanese2000@gmail.com>
2020-08-03 16:26:28 +0200

    Made methods suspend. Stubbed method to register user with firebase.

--- app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginDataSource.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/Login/LoginDataSource.kt
@@ -4,11 +4,16 @@ import java.util.*
 
 class LoginDataSource(val loginDao: LoginDao) {
 
-
-    fun registerUser(registrationRequest: RegistrationRequest?) {
+    suspend fun registerUser(registrationRequest: RegistrationRequest?): Long {
         val uuid = UUID.randomUUID()
         val agent = Agent(0, uuid, registrationRequest?.firstName, registrationRequest?.lastName, registrationRequest?.email, registrationRequest?.password, registrationRequest?.passwordHint
                 ?: "")
+        return loginDao.registerAgent(agent)
     }
+
+    suspend fun registerAgentFirebase(agent: Agent) {
+
+    }
+
 
 }
